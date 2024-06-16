9f47a82803e27f39d7922c80dae3f1421de88f90
Alokamrish <alokc273155@gmail.com>
2024-06-17 01:02:18 +0530

    Initial commit

---
+++ members/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from members.views import *
+
+urlpatterns = [
+    path('', views.home,name='josaa-home'),
+    path('contact/', views.contact1,name='josaa-contact'),
+    path('about/', views.about,name='josaa-about'),
+    path('iitList/', views.iitList,name='iit-list'),
+    # path('iitb/',views.iitb,name='iitb'),
+    path('iitb/<str:college>/', views.iitb, name='iitb'),
+    path('iitmech/<str:college>/', views.iitmech, name='iitmech'),
+    
+    path('insti_cutoff/', views.insti_cutoff, name='insti_cutoff'),
+    # path('iitb/',AdmissionChartView.as_view(),name='iitb'),
+]
